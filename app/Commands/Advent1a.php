afdbb09f76c9a7ab6ec8b368616078cc43e0b25d
Cor Bosman <cor@xs4all.net>
2021-12-01 12:23:51 +0100

    wip

---
+++ app/Commands/Advent1a.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class Advent1a extends Command
+{
+    protected $signature = '1a';
+    protected $description = 'Advent 1a';
+
+    public function handle()
+    {
+        $this->info(collect(file(storage_path('1.txt'), FILE_IGNORE_NEW_LINES))
+            ->sliding(2)
+            ->filter(fn ($window) => $window->first() < $window->last())
+            ->count()
+        );
+    }
+}
