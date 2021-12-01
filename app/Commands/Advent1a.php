bdae39582fa90a972e9d2de105d46cb690fbb799
Cor Bosman <cor@xs4all.net>
2021-12-01 12:30:37 +0100

    wip

--- app/Commands/Advent1a.php
+++ app/Commands/Advent1a.php
@@ -11,7 +11,8 @@ class Advent1a extends Command
 
     public function handle()
     {
-        $this->info(collect(file(storage_path('1.txt'), FILE_IGNORE_NEW_LINES))
+        $this->info(
+            collect(file(storage_path('1.txt'), FILE_IGNORE_NEW_LINES))
             ->sliding(2)
             ->filter(fn ($window) => $window->first() < $window->last())
             ->count()
