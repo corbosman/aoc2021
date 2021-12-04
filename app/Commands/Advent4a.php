8fd756afb205d73473de05708dfbf90f2562db19
Cor Bosman <cor@xs4all.net>
2021-12-04 20:34:23 +0100

    wip

--- app/Commands/Advent4a.php
+++ app/Commands/Advent4a.php
@@ -16,7 +16,7 @@ class Advent4a extends Command
 
         [$winner, $number] = $this->play($random, $boards);
 
-        $unmarked_sum = $winner->flatten()->filter(fn($v) => is_int($v))->sum();
+        $unmarked_sum = collect($winner)->flatten()->filter()->sum();
 
         $this->info("final score = " . $unmarked_sum * $number);
     }
@@ -36,23 +36,20 @@ class Advent4a extends Command
     /**
      * mark numbers on the boards
      */
-    public function mark($number, $boards) : Collection
+    public function mark($number, $boards) : array
     {
-        return $boards->map(fn($board) => $board->map(fn($row) => array_map(fn($v) => $v === $number ? 'X' : $v, $row)));
+        return map(fn($board) => map(fn($row) => map(fn($v) => $v === $number ? false : $v, $row), $board), $boards);
     }
 
     /**
      * Check if any board is a winner
      */
-    public function winner($boards) : ?Collection
+    public function winner($boards) : ?array
     {
        foreach($boards as $board) {
            foreach(range(0,4) as $i) {
-               /* check row */
-               if (count(array_filter($board[$i], fn($v) => $v !== 'X')) === 0) return $board;
-
-               /* check column */
-               if (count(array_filter(array_column($board->toArray(), $i), fn($v) => $v !== 'X')) === 0) return $board;
+               if (count(array_filter($board[$i], fn($v) => $v !== false)) === 0) return $board;
+               if (count(array_filter(array_column($board, $i), fn($v) => $v !== false)) === 0) return $board;
            }
        }
 
@@ -64,7 +61,7 @@ class Advent4a extends Command
      */
     public function read_input() : array
     {
-        $input = collect(file(storage_path('4e.txt'), FILE_IGNORE_NEW_LINES));
+        $input = collect(file(storage_path('4.txt'), FILE_IGNORE_NEW_LINES));
 
         $random = map('intval', explode(',', $input[0]));
 
@@ -72,7 +69,8 @@ class Advent4a extends Command
             ->splice(2)                                                                                                                // skip first 2 lines
             ->filter(fn($l) => $l !== "")                                                                                              // remove empty lines
             ->chunk(5)                                                                                                                 // chop into sections of 5 lines
-            ->map(fn($board) => $board->map(fn($row) => array_map(fn($v) => (int)$v, preg_split('/\s+/', trim($row))))->values());     // convert into 2 dimensional array
+            ->map(fn($board) => $board->map(fn($row) => array_map(fn($v) => (int)$v, preg_split('/\s+/', trim($row))))->values())
+            ->toArray();
 
         return [$random, $boards];
     }
