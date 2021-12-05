ddaeef3e7b9865632c13229863c8005ebf64beed
Cor Bosman <cor@xs4all.net>
2021-12-05 14:33:45 +0100

    wip

--- src/5/5b.php
+++ src/5/5b.php
@@ -5,11 +5,11 @@ require __DIR__ . '/../../vendor/autoload.php';
 $vents = collect(file('input.txt', FILE_IGNORE_NEW_LINES))
     ->map(function($i) { preg_match_all('/(\d+)/', $i, $array); return $array[0]; })
     ->map(fn($i) => map(fn($i) => (int)$i, $i))
-    ->reduce(function($carry, $i) {
+    ->reduce(function($points, $i) {
         foreach(points($i[0], $i[1], $i[2], $i[3]) as $p) {
-            $carry[$p[0]][$p[1]] = isset($carry[$p[0]][$p[1]]) ? $carry[$p[0]][$p[1]]+1 : 1;
+            $points[$p[0]][$p[1]] = isset($points[$p[0]][$p[1]]) ? $points[$p[0]][$p[1]]+1 : 1;
         }
-        return $carry;
+        return $points;
     }, []);
 
 output(collect($vents)->flatten()->filter(fn($i) => $i > 1)->count());
