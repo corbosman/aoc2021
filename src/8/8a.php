6ba18e13b1ce590807d8a3dfb4f580ee31f524ea
Cor Bosman <cor@xs4all.net>
2021-12-08 11:27:49 +0100

    wip

---
+++ src/8/8a.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+$count = collect(collect(file('input.txt', FILE_IGNORE_NEW_LINES)))
+    ->map(fn($i)=>explode(' ', preg_split('/ \| /', $i)[1]))->flatten()
+    ->filter(fn($i)=>in_array(strlen($i),[2,3,4,7]))
+    ->count();
+
+output("count={$count}");
