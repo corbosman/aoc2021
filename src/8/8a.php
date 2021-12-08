8ca565c92192638f4cf8515546797430854cbfd9
Cor Bosman <cor@xs4all.net>
2021-12-08 11:28:25 +0100

    wip

--- src/8/8a.php
+++ src/8/8a.php
@@ -2,7 +2,7 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
 
-$count = collect(collect(file('input.txt', FILE_IGNORE_NEW_LINES)))
+$count = collect(file('input.txt', FILE_IGNORE_NEW_LINES))
     ->map(fn($i)=>explode(' ', preg_split('/ \| /', $i)[1]))->flatten()
     ->filter(fn($i)=>in_array(strlen($i),[2,3,4,7]))
     ->count();
