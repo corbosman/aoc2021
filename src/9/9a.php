150cf260bb2e4eb83181b4806ae11f0875b7af72
Cor Bosman <cor@xs4all.net>
2021-12-09 12:23:36 +0100

    wip

--- src/9/9a.php
+++ src/9/9a.php
@@ -7,9 +7,9 @@ $width = count($input[0])-1;
 $height = count($input)-1;
 
 $level = $input->mapWithKeys(fn($r,$i) => [$i => $r->filter(fn($h, $j) => ($i == 0 or $h < $input[$i-1][$j]) and
-                                                                           ($j == 0 or $h < $input[$i][$j-1]) and
-                                                                           ($j == $width or $h < $input[$i][$j+1]) and
-                                                                           ($i == $height or $h < $input[$i+1][$j]))]
+                                                                          ($j == 0 or $h < $input[$i][$j-1]) and
+                                                                          ($j == $width or $h < $input[$i][$j+1]) and
+                                                                          ($i == $height or $h < $input[$i+1][$j]))]
 )->flatten()->map(fn($i)=>$i+1)->sum();
 
 output($level);
