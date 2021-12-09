7a1dd66e9d4e368febf37e831b64f707571917e9
Cor Bosman <cor@xs4all.net>
2021-12-09 12:16:12 +0100

    wip

--- src/9/9a.php
+++ src/9/9a.php
@@ -6,11 +6,10 @@ $input = collect(file('input.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>collect(
 $width = count($input[0])-1;
 $height = count($input)-1;
 
-$output = $input->mapWithKeys(
-    fn($r,$i) => [$i => $r->filter(fn($h, $j) => ($i == 0 or $h < $input[$i-1][$j]) and
-                                                 ($j == 0 or $h < $input[$i][$j-1]) and
-                                                 ($j == $width or $h < $input[$i][$j+1]) and
-                                                 ($i == $height or $h < $input[$i+1][$j]))]
+$output = $input->mapWithKeys(fn($r,$i) => [$i => $r->filter(fn($h, $j) => ($i == 0 or $h < $input[$i-1][$j]) and
+                                                                           ($j == 0 or $h < $input[$i][$j-1]) and
+                                                                           ($j == $width or $h < $input[$i][$j+1]) and
+                                                                           ($i == $height or $h < $input[$i+1][$j]))]
 )->flatten()->map(fn($i)=>$i+1)->sum();
 
 output($output);
