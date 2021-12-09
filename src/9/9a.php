79ea62010316e71fd95b3e257e00c85f2584f35d
Cor Bosman <cor@xs4all.net>
2021-12-09 12:12:06 +0100

    wip

---
+++ src/9/9a.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+$input = collect(file('input.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>collect(str_split($i)));
+$width = count($input[0])-1;
+$height = count($input)-1;
+
+$output = $input->mapWithKeys(
+    fn($r,$i) => [
+        $i => $r->filter(fn($h, $j) => ($i == 0 or $h < $input[$i-1][$j]) and ($j == 0 or $h < $input[$i][$j-1]) and ($j == $width or $h < $input[$i][$j+1]) and ($i == $height or $h < $input[$i+1][$j]))
+    ]
+)->flatten()->map(fn($i)=>$i+1)->sum();
+
+output($output);
