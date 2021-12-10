d85906ee72ab1110c201eb75a89c559b372cf369
Cor Bosman <cor@xs4all.net>
2021-12-10 15:01:33 +0100

    wip

--- src/10/10b.php
+++ src/10/10b.php
@@ -4,54 +4,33 @@ use Tightenco\Collect\Support\Collection;
 require __DIR__ . '/../../vendor/autoload.php';
 
 $input = collect(file('input.txt', FILE_IGNORE_NEW_LINES));
-$winner = (new Parser)->parse($input);
+$incomplete = collect();
 
-output($winner);
-
-
-class Parser
-{
-    public function parse($input)
-    {
-        $incomplete   = $this->process($input);
-        $score        = $this->score($incomplete);
-
-        return $score->sort()->values()[$score->count() / 2];
-    }
-
-    private function process($array) : Collection
-    {
-        $processed  = collect();
-
-        foreach($array as $line) {
-            $stack = collect();
-            foreach(str_split($line) as $chr) {
-                if (str_contains('({[<', $chr)) {
-                    $stack->push($chr);
-                } else {
-                    if ($stack->pop() !== $this->match($chr)) continue 2;
-                }
-            }
-            $processed->push($stack);
+foreach($input as $line) {
+    $stack = collect();
+    foreach(str_split($line) as $chr) {
+        if (str_contains('({[<', $chr)) {
+            $stack->push($chr);
+        } else {
+            if ($stack->pop() !== pair($chr)) continue 2;
         }
-        return $processed;
     }
-
-    public function score($array) : Collection
-    {
-        return $array->map(fn($line) => $line->reverse()->reduce(function($score, $chr) {
-           return ($score * 5) + match($chr) {
-               '(' => 1, '[' => 2, '{' => 3, '<' => 4
-           };
-        }, 0))->values();
-    }
-
-    private function match($chr) : string
-    {
-        return match($chr) {
-            '[' => ']', ']' => '[', '{' => '}', '}' => '{', '<' => '>', '>' => '<', '(' => ')', ')' => '('
-        };
-    }
-
+    $incomplete->push($stack);
 }
+
+$score = $incomplete->map(fn($line) => $line->reverse()->reduce(function($score, $chr) {
+    return ($score * 5) + match($chr) {
+            '(' => 1, '[' => 2, '{' => 3, '<' => 4
+        };
+}, 0))->sort()->values();
+
+output($score[count($score)/2]);
+
+function pair($chr) : string
+{
+    return match($chr) {
+        '[' => ']', ']' => '[', '{' => '}', '}' => '{', '<' => '>', '>' => '<', '(' => ')', ')' => '('
+    };
+}
+
 
