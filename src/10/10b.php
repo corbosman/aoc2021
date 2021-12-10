f9479175700b25ddce6788f599176f97326b08cc
Cor Bosman <cor@xs4all.net>
2021-12-10 15:05:40 +0100

    wip

--- src/10/10b.php
+++ src/10/10b.php
@@ -4,25 +4,22 @@ use Tightenco\Collect\Support\Collection;
 require __DIR__ . '/../../vendor/autoload.php';
 
 $input = collect(file('input.txt', FILE_IGNORE_NEW_LINES));
-$incomplete = collect();
 
-foreach($input as $line) {
+$score = $input->reduce(function($incomplete, $line) {
     $stack = collect();
     foreach(str_split($line) as $chr) {
         if (str_contains('({[<', $chr)) {
             $stack->push($chr);
         } else {
-            if ($stack->pop() !== pair($chr)) continue 2;
+            if ($stack->pop() !== pair($chr)) return $incomplete;
         }
     }
-    $incomplete->push($stack);
-}
-
-$score = $incomplete->map(fn($line) => $line->reverse()->reduce(function($score, $chr) {
+    return $incomplete->push($stack);
+}, collect())->map(fn($line) => $line->reverse()->reduce(function($score, $chr) {
     return ($score * 5) + match($chr) {
             '(' => 1, '[' => 2, '{' => 3, '<' => 4
         };
-}, 0))->sort()->values();
+}, 0))->sort()->values();;
 
 output($score[count($score)/2]);
 
