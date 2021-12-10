22dee6ae4aaf3d767c83fb31658520173e7f1b2c
Cor Bosman <cor@xs4all.net>
2021-12-10 12:07:34 +0100

    wip

---
+++ src/10/10a.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+use Tightenco\Collect\Support\Collection;
+require __DIR__ . '/../../vendor/autoload.php';
+
+$input = collect(file('input.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>str_split($i));
+
+$brackets = [']' => '[', '}' => '{', ')' => '(', '>' => '<'];
+$score    = [')' => 3, ']' => 57, '}' => 1197, '>' => 25137];
+$errors   = collect();
+
+foreach($input as $line) {
+    $stack = collect();
+    foreach($line as $nav) {
+        if (in_array($nav, $brackets)) {
+            $stack->push($nav);
+        } else {
+            $last = $stack->pop();
+            if ($last !== $brackets[$nav]) {
+                $errors->push($score[$nav]);
+                break;
+            }
+        }
+    }
+}
+
+output($errors->sum());
+
