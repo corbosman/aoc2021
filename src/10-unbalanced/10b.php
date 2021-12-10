c5ce0b922478e92af77bce818ed7e39eeaad57a5
Cor Bosman <cor@xs4all.net>
2021-12-10 15:25:58 +0100

    wip

--- src/10-unbalanced/10b.php
+++ src/10-unbalanced/10b.php
@@ -2,16 +2,16 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
 
-$input = collect(file('input.txt', FILE_IGNORE_NEW_LINES));
+$input = collect(file('input_e.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>collect(str_split($i)));
 
 $score = $input->reduce(function($incomplete, $line) {
-    $stack = collect();
-    foreach(str_split($line) as $chr) {
-        if (str_contains('({[<', $chr)) $stack->push($chr);
-        elseif ($stack->pop() !== pair($chr)) return $incomplete;
-    }
-    return $incomplete->push($stack);
-}, collect())
+            $stack = collect();
+            foreach(str_split($line) as $chr) {
+                if (str_contains('({[<', $chr)) $stack->push($chr);
+                elseif ($stack->pop() !== pair($chr)) return $incomplete;
+            }
+            return $incomplete->push($stack);
+        }, collect())
     ->map(fn($line) => $line->reverse()->reduce(fn($score, $chr) => ($score * 5) + match($chr) { '(' => 1, '[' => 2, '{' => 3, '<' => 4 }, 0))
     ->sort()
     ->values();
