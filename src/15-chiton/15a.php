5cb40f6c42060ec0973c988c0d42c4a556e5a664
Cor Bosman <cor@xs4all.net>
2021-12-15 15:27:55 +0100

    wip

--- src/15-chiton/15a.php
+++ src/15-chiton/15a.php
@@ -4,37 +4,83 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 function load() : array
 {
-    $input = collect(file('input.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>str_split($i));
+    $input = collect(file('input_e.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>str_split($i));
     return $input->toArray();
 }
 
-function calculate_risk($cave) : array
+function expand(array $cave) : array
 {
-    $height = count($cave);
-    $width = count($cave[0]);
-    $risk_map = [];
+    $size = count($cave);
+    $expanded = [];
 
-    for($x=$height-1; $x>=0; $x--) {
-        for($y=$width-1; $y>=0; $y--) {
-            $current_risk = $cave[$x][$y];
-
-            $bottom_risk  = $x+1 >= $height ? null : $risk_map[$x+1][$y];
-            $right_risk   = $y+1 >= $width ? null : $risk_map[$x][$y+1];
-
-            if (!$bottom_risk && !$right_risk) {
-                $lowest_risk = 0;
-            } else {
-                $lowest_risk = !$bottom_risk ? $right_risk : (!$right_risk ? $bottom_risk : min($bottom_risk, $right_risk));
+    for($i=0;$i<5;$i++) {
+        for($j=0;$j<5;$j++) {
+            $add = $i+$j;
+            foreach($cave as $x => $row) {
+                foreach ($row as $y => $v) {
+                    $risk = ($cave[$x][$y]+$add);
+                    while($risk > 9) $risk = $risk - 9;
+                    $expanded[$x+($i*$size)][$y+($j*$size)] = $risk;
+                }
             }
-            $risk_map[$x][$y] = $current_risk + $lowest_risk;
         }
     }
-    return $risk_map;
+    return $expanded;
+}
+
+function lowest(?int $risk1, ?int $risk2) : int
+{
+    if (!$risk1 && !$risk2) return 0;
+    if (!$risk1) return $risk2;
+    if (!$risk2) return $risk1;
+    return min($risk1, $risk2);
+}
+
+function lowest_risk_level(array $cave) : int
+{
+    $max      = count($cave)-1;
+    $risk_map = [];
+
+    for($x=0; $x<=$max; $x++) {
+        for($y=0; $y<=$max; $y++) {
+            $current_risk = $cave[$x][$y];
+
+            $top_risk  = $x-1<0 ? null : $risk_map[$x-1][$y];
+            $left_risk = $y-1<0 ? null : $risk_map[$x][$y-1];
+
+            $risk_map[$x][$y] = $current_risk + lowest($left_risk, $top_risk);
+        }
+    }
+    print_risk_map($cave, $risk_map);
+    return $risk_map[$max][$max] - $cave[0][0];
 }
 
 $cave = load();
-$risk_map = calculate_risk($cave);
-$risk_for_top_left = $risk_map[0][0] - $cave[0][0];
+//$risk = lowest_risk_level($cave);
+//output("15a = {$risk}");
 
-output($risk_for_top_left);
+$cave = expand($cave);
+$risk2 = lowest_risk_level($cave);
+output("15b = {$risk2}");
 
+
+function print_cave($cave)
+{
+    for($x=0; $x<count($cave); $x++) {
+        for($y=0; $y<count($cave[0]); $y++) {
+            echo sprintf("%1s", $cave[$x][$y]);
+        }
+        echo "\n";
+    }
+}
+
+function print_risk_map($cave, $risk_map)
+{
+    for($x=0; $x<count($cave); $x++) {
+        for($y=0; $y<count($cave[0]); $y++) {
+            $risk = $risk_map[$x][$y] ?? '.';
+            echo sprintf("%5s", $risk);
+        }
+        echo "\n";
+    }
+}
