c00a3b463c99ef2e853a64b7ebee1a21d6cf4df0
Cor Bosman <cor@xs4all.net>
2021-12-16 13:55:49 +0100

    wip

--- src/15-chiton/15a.php
+++ src/15-chiton/15a.php
@@ -6,7 +6,7 @@ const INFINITE = 999999999999999;
 
 function load() : array
 {
-    $input = collect(file('input_e.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>str_split($i));
+    $input = collect(file('input_small.txt', FILE_IGNORE_NEW_LINES))->map(fn($i)=>str_split($i));
     return $input->toArray();
 }
 
@@ -30,28 +30,112 @@ function expand(array $cave) : array
     return $expanded;
 }
 
-function lowest(?int $risk1, ?int $risk2) : int
+function create_unvisited(array $cave) : array
 {
-    if (!$risk1 && !$risk2) return 0;
-    if (!$risk1) return $risk2;
-    if (!$risk2) return $risk1;
-    return min($risk1, $risk2);
+    $unvisited = [];
+    foreach($cave as $x => $v1) {
+        foreach($v1 as $y => $v2) {
+            $unvisited[$x][$y] = INFINITE;
+        }
+    }
+    // initialise the first unvisited distance
+    $unvisited[0][0] = $cave[0][0];
+    return $unvisited;
 }
 
-function lowest_risk_level(array $cave, $x, $y, $size, &$risk_map, &$lowest) : int
+function next_node(array $unvisited) : array
 {
+    $next = [];
+    $min = INFINITE;
+    foreach($unvisited as $x => $row) {
+        foreach($row as $y => $distance) {
+            if ($distance < $min) {
+                $min = $distance;
+                $next = [$x,$y];
+            }
+        }
+    }
+    return $next;
+ }
 
+ function register_visit($x, $y, &$visited, &$unvisited) : void
+ {
+     $visited[$x][$y] = true;
+     unset($unvisited[$x][$y]);
+     if (count($unvisited[$x]) === 0) unset($unvisited[$x]);
+ }
+
+function neighbors(array $cave, int $x, int $y, int $width, int $height, $visited) : array
+{
+    $neighbors = [];
+
+    foreach ([[1,0], [0,1], [-1,0], [0,-1]] as list($dx, $dy)) {
+        if ($x+$dx >= 0 && $x+$dx < $height && $y+$dy >= 0 && $y+$dy < $height) {
+            $neighbors[] = [$x+$dx, $y+$dy];
+        }
+    }
+
+    return $neighbors;
+}
+
+function dijkstra(array $cave, $x, $y) : int
+{
+    $unvisited = create_unvisited($cave);
+    dump("unvisited=" . count($unvisited));
+    $loop = 1;
+    $visited   = [];
+    $distances = [0=>[0=>1]];
+    $width     = count($cave[0]);
+    $height    = count($cave);
+    $lowest_risk = INFINITE;
+
+    while (count($unvisited) > 0) {
+
+        dump("{$x}{$y}");
+        // find all the neighbors for the current position
+        $neighbors = neighbors($cave, $x, $y, $width, $height, $visited);
+
+        // foreach neighbor, calculate the new distance and check with previous known minimal distance
+        foreach ($neighbors as list($nx, $ny)) {
+            if (isset($visited[$nx][$ny])) {
+                dump("already visited {$nx}{$ny}");
+                continue;
+            }
+
+            $distance   = $distances[$x][$y] + $cave[$nx][$ny];
+            $distance_n = $distances[$nx][$ny] ?? INFINITE;
+            if ($distance < $distance_n) {
+                $distances[$nx][$ny] = $distance;
+                $unvisited[$nx][$ny] = $distance;
+            }
+        }
+
+        // mark current node as visited
+        register_visit($x, $y, $visited, $unvisited);
+
+        if (count($unvisited) > 0) {
+            list($x, $y) = next_node($unvisited);
+        }
+        print_risk_map($cave, $distances);
+        $loop += 1;
+        if ($loop % 1) {
+            print_risk_map($cave, $distances);
+        }
+    }
+
+    // print_risk_map($cave, $distances);
+
+    return $distances[$height-1][$width-1] - $distances[0][0];
 }
 
 $cave = load();
-$size = count($cave);
-
-$risk = lowest_risk_level($cave, $cache, INFINITE);
-output("15a = {$risk}");
+$minimum_risk = dijkstra($cave, 0, 0);
+output("15a={$minimum_risk}");
 
 $cave = expand($cave);
-$risk2 = lowest_risk_level($cave);
-output("15b = {$risk2}");
+$minimum_risk = dijkstra($cave, 0, 0);
+output("15b={$minimum_risk}");
+
 
 
 function print_cave($cave)
@@ -64,13 +148,19 @@ function print_cave($cave)
     }
 }
 
-function print_risk_map($cave, $risk_map)
+function print_risk_map($cave, $distances)
 {
-    for($x=0; $x<count($cave); $x++) {
+    for($i=0;$i<count($cave); $i++){
+        echo sprintf("%5s", $i);
+    }
+    echo "\n--------------------------------------------------------\n";
+    for($x=0; $x<count($distances); $x++) {
         for($y=0; $y<count($cave[0]); $y++) {
-            $risk = $risk_map[$x][$y] ?? '.';
+            $risk = $distances[$x][$y] ?? '.';
             echo sprintf("%5s", $risk);
         }
+        echo " | $x";
         echo "\n";
     }
+    echo "\n";
 }
