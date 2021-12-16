4f166684ca9842464112722be036e49bea1af18c
Cor Bosman <cor@xs4all.net>
2021-12-16 15:22:46 +0100

    wip

--- src/15-chiton/print.php
+++ src/15-chiton/print.php
@@ -10,3 +10,20 @@ function print_cave($cave, $risk_map)
         echo "\n";
     }
 }
+
+function print_risk_map($cave, $distances)
+{
+    for($i=0;$i<count($cave); $i++){
+        echo sprintf("%5s", $i);
+    }
+    echo "\n--------------------------------------------------------\n";
+    for($x=0; $x<count($distances); $x++) {
+        for($y=0; $y<count($cave[0]); $y++) {
+            $risk = $distances[$x][$y] ?? '.';
+            echo sprintf("%5s", $risk);
+        }
+        echo " | $x";
+        echo "\n";
+    }
+    echo "\n";
+}
