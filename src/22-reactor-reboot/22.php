838cad3eeed50667bee7f7cba400a658efff6e6c
Cor Bosman <cor@xs4all.net>
2021-12-23 18:11:12 +0100

    wip

--- src/22-reactor-reboot/22.php
+++ src/22-reactor-reboot/22.php
@@ -1,20 +1,21 @@
 #!/usr/bin/env php
 <?php
 use Tightenco\Collect\Support\Collection;
+ini_set('memory_limit','2G');
+CONST LIMIT = 50;
 
 require __DIR__ . '/../../vendor/autoload.php';
 $time1              = microtime(true);
 
 function load() : Collection
 {
-    return collect(input('inputs/input_e2.txt'))->map(function($line) {
+    return collect(input('inputs/input.txt'))->map(function($line) {
         preg_match('/^(.*) x=(-?[0-9]+)..(-?[0-9]+),y=(-?[0-9]+)..(-?[0-9]+),z=(-?[0-9]+)..(-?[0-9]+)$/', $line, $m);
         return [
             'command' => $m[1],
-            'x' => [(int)$m[2], (int)$m[3]],
-            'y' => [(int)$m[4], (int)$m[5]],
-            'z' => [(int)$m[6], (int)$m[7]],
-            'outside_boundary' => $m[2]>50||$m[3]>50||$m[4]>50||$m[5]>50||$m[6]>50||$m[7]>50
+            'x' => [max($m[2], -LIMIT), min($m[3], LIMIT)],
+            'y' => [max($m[4], -LIMIT), min($m[5], LIMIT)],
+            'z' => [max($m[6], -LIMIT), min($m[7], LIMIT)],
         ];
     });
 }
@@ -26,11 +27,11 @@ class Reactor
     public function reboot($steps)
     {
         foreach($steps as $step) {
-            foreach(range($step['x'][0], $step['x'][1]) as $x) {
-                foreach(range($step['y'][0], $step['y'][1]) as $y) {
-                    foreach(range($step['z'][0], $step['z'][1]) as $z) {
-                        if ($step['command'] === 'on') $this->turn_on($x, $y, $z);
-                        if ($step['command'] === 'off') $this->turn_off($x, $y, $z);
+            for($x=$step['x'][0];$x<=$step['x'][1]; $x++) {
+                for($y=$step['y'][0];$y<=$step['y'][1]; $y++) {
+                    for($z=$step['z'][0];$z<=$step['z'][1]; $z++) {
+                        if ($step['command'] === 'on') $this->cubes[$x][$y][$z] = 1;
+                        if ($step['command'] === 'off') $this->cubes[$x][$y][$z] = 0;
                     }
                 }
             }
@@ -38,23 +39,13 @@ class Reactor
         return $this;
     }
 
-    protected function turn_on($x, $y, $z)
+    public function count_cubes() : int
     {
-       $this->cubes[$x][$y][$z] = true;
-    }
-
-    protected function turn_off($x, $y, $z)
-    {
-        unset($this->cubes[$x][$y][$z]);
-    }
-
-    public function cubes() : array
-    {
-        $cubes = [];
+        $cubes = 0;
         foreach($this->cubes as $x => $xx) {
             foreach($xx as $y => $yy) {
                 foreach($yy as $z => $state) {
-                    if ($state === true) $cubes[] = [$x, $y, $z];
+                    $cubes += $this->cubes[$x][$y][$z];
                 }
             }
         }
@@ -63,11 +54,8 @@ class Reactor
 }
 
 
-$input = load();
-$steps = $input->filter(fn($step) => !$step['outside_boundary']);
-
+$steps   = load();
 $reactor = new Reactor;
-$cubes = $reactor->reboot($steps)->cubes();
-dd($cubes);
-$time2              = microtime(true);
-solution(0, $time1, $time2, '22a');
+$cubes   = $reactor->reboot($steps)->count_cubes();
+$time2   = microtime(true);
+solution($cubes, $time1, $time2, '22a');
