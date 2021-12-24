6ab733aee39043047093a6f369ef84a9855a78f0
Cor Bosman <cor@xs4all.net>
2021-12-25 00:47:28 +0100

    wip

--- src/22-reactor-reboot/22b.php
+++ src/22-reactor-reboot/22b.php
@@ -54,12 +54,29 @@ class Reactor
         [$cx1, $cx2, $cy1, $cy2, $cz1, $cz2] = $cube;
 
         unset($this->cubes[$i]);
-        if ($x2 < $cx2) $this->cubes[] = [$x2 + 1, $cx2, $cy1, $cy2, $cz1, $cz2];
-        if ($x1 > $cx1) $this->cubes[] = [$cx1, $x1 - 1, $cy1, $cy2, $cz1, $cz2];
-        if ($y2 < $cy2) $this->cubes[] = [max($x1, $cx1), min($cx2, $x2), $y2 + 1, $cy2, $cz1, $cz2];
-        if ($y1 > $cy1) $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), $cy1, $y1 - 1, $cz1, $cz2];
-        if ($z2 < $cz2) $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), max($cy1, $y1), min($cy2, $y2), $z2 + 1, $cz2];
-        if ($z1 > $cz1) $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), max($cy1, $y1), min($cy2, $y2), $cz1, $z1 - 1];
+        if ($x2 < $cx2) {
+            $this->cubes[] = [$x2 + 1, $cx2, $cy1, $cy2, $cz1, $cz2];
+        }
+
+        if ($x1 > $cx1) {
+            $this->cubes[] = [$cx1, $x1 - 1, $cy1, $cy2, $cz1, $cz2];
+        }
+
+        if ($y2 < $cy2) {
+            $this->cubes[] = [max($x1, $cx1), min($cx2, $x2), $y2 + 1, $cy2, $cz1, $cz2];
+        }
+
+        if ($y1 > $cy1) {
+            $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), $cy1, $y1 - 1, $cz1, $cz2];
+        }
+
+        if ($z2 < $cz2) {
+            $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), max($cy1, $y1), min($cy2, $y2), $z2 + 1, $cz2];
+        }
+
+        if ($z1 > $cz1) {
+            $this->cubes[] = [max($cx1, $x1), min($cx2, $x2), max($cy1, $y1), min($cy2, $y2), $cz1, $z1 - 1];
+        }
     }
 
     public function count_cubes() : int
